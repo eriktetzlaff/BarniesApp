d15b1e7d597f887cb2e0f9452dc6aab036e34262
Erik Tetzlaff <erik_tetzlaff@web.de>
2025-03-29 19:51:25 +0100

    First HTML Template

--- barnies_app/urls.py
+++ barnies_app/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mylist.views import list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('list/', list),
 ]
