9cad32bf5ef8f2cf05907fc81ff380e86eed86cf
Erik Tetzlaff <erik_tetzlaff@web.de>
2025-04-04 21:55:28 +0200

    Tutorial Done

--- mylist/views.py
+++ mylist/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from .models import ShoppingItem
 # Create your views here.
 def list(request):
-    return render(request, 'list.html')
+    if request.method == 'POST':
+        print('Received data:', request.POST['itemName'])
+        ShoppingItem.objects.create(name = request.POST['itemName'])
+    all_items = ShoppingItem.objects.all()
+    return render(request, 'list.html', {'all_items': all_items})
