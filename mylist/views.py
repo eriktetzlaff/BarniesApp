ff623985323b2308f7f948a0d1353dca3963c0db
Erik Tetzlaff <erik_tetzlaff@web.de>
2025-04-10 19:11:41 +0200

    Delete Funktion

--- mylist/views.py
+++ mylist/views.py
@@ -3,7 +3,13 @@ from .models import ShoppingItem
 # Create your views here.
 def list(request):
     if request.method == 'POST':
-        print('Received data:', request.POST['itemName'])
-        ShoppingItem.objects.create(name = request.POST['itemName'])
+        action = request.POST.get('action')
+        itemName = request.POST.get('itemName')
+        if(action == 'delete'):
+            ShoppingItem.objects.filter(name = itemName).delete()
+            print('Deleted data:', request.POST['itemName'])
+        elif(action == 'create'):
+            print('Received data:', request.POST['itemName'])
+            ShoppingItem.objects.create(name = request.POST['itemName'])
     all_items = ShoppingItem.objects.all()
     return render(request, 'list.html', {'all_items': all_items})
